6888047650a8871266491e506bd2e3794cf3badd
haider ali <ha347278@gmail.com>
2022-06-08 00:43:10 +0500

    strings are added in second activity and list view activity is added

--- app/src/main/java/com/example/kidsgame/secondA.java
+++ app/src/main/java/com/example/kidsgame/secondA.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class secondA extends AppCompatActivity {
 
+    String fruitList [] ={"apple","banana","cat","dog","egg","fish"};
+    int fruitImages [] ={R.drawable.apple,R.drawable.banana,R.drawable.cat,R.drawable.dog,R.drawable.egg,R.drawable.fish};
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
