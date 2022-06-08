7bf41a802c28232ca2571e037fbaad62181b83da
haider ali <ha347278@gmail.com>
2022-06-08 21:25:23 +0500

    adapter class

--- app/src/main/java/com/example/kidsgame/secondA.java
+++ app/src/main/java/com/example/kidsgame/secondA.java
@@ -7,16 +7,18 @@ import android.widget.ListView;
 
 public class secondA extends AppCompatActivity {
     ListView lv;
-    String []name={"A for Apple","B for Banana","C for Cat","D for Dog","E for Egg","F for Fish","G for Glue"};
-
-    Integer[]img={R.drawable.apple,R.drawable.banana,R.drawable.cat,R.drawable.dog,R.drawable.egg,R.drawable.fish};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.lv);
 
+        String []name={"Facebook","Instagram"};
+        Integer[]img={R.drawable.ic_launcher_background,R.drawable.ic_launcher_foreground};
+
+        MyListAdapter adapter=new MyListAdapter(this, name,img);
+        lv.setAdapter(adapter);
 
     }
 }
