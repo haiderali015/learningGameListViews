407f71ead48d7d0903ebc690c832c3291bdaa348
haider ali <ha347278@gmail.com>
2022-06-08 21:33:38 +0500

    things settled

--- app/src/main/java/com/example/kidsgame/secondA.java
+++ app/src/main/java/com/example/kidsgame/secondA.java
@@ -2,20 +2,27 @@ package com.example.kidsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-public class secondA extends AppCompatActivity {
+public class secondA extends MainActivity {
     ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         lv=findViewById(R.id.lv);
 
-        String []name={"Facebook","Instagram"};
-        Integer[]img={R.drawable.ic_launcher_background,R.drawable.ic_launcher_foreground};
+        String []name={"A for Apple","B for Banana","C for Cat","D for dog","E for Egg"};
+        Integer[]img={R.drawable.apple,R.drawable.banana,R.drawable.cat,R.drawable.dog,R.drawable.egg};
 
         MyListAdapter adapter=new MyListAdapter(this, name,img);
         lv.setAdapter(adapter);
@@ -35,4 +42,20 @@ public class secondA extends AppCompatActivity {
             this.img = img;
             this.name=name;
         }
+
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater=context.getLayoutInflater();
+            View rowView=inflater.inflate(R.layout.customlist, null,true);
+
+            TextView titleText = (TextView) rowView.findViewById(R.id.title);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+            titleText.setText(name[position]);
+            imageView.setImageResource(img[position]);
+
+            return rowView;
+
+        };
+    }
+
 }
