fd8b7c72907634fa5babc90506458d5f615bd77d
haider ali <ha347278@gmail.com>
2022-06-08 20:54:14 +0500

    some changes made

--- app/src/main/java/com/example/kidsgame/secondA.java
+++ app/src/main/java/com/example/kidsgame/secondA.java
@@ -3,15 +3,21 @@ package com.example.kidsgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class secondA extends AppCompatActivity {
 
     String fruitList [] ={"apple","banana","cat","dog","egg","fish"};
     int fruitImages [] ={R.drawable.apple,R.drawable.banana,R.drawable.cat,R.drawable.dog,R.drawable.egg,R.drawable.fish};
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        listView = (ListView) findViewById(R.id.customListView);
+        custombaseAdapter cstmbaseAdapter = new custombaseAdapter(getApplicationContext(),fruitList,fruitImages);
+
+        listView.setAdapter(custombaseAdapter);
+
     }
 }
