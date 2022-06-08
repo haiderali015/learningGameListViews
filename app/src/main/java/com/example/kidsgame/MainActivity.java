8e1829ed21b1fd2413755371b038b112bc589645
haider ali <ha347278@gmail.com>
2022-06-08 21:14:35 +0500

    array adapter class created

--- app/src/main/java/com/example/kidsgame/MainActivity.java
+++ app/src/main/java/com/example/kidsgame/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.kidsgame;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -19,7 +21,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.lv);
 
+        String []name={"Facebook","Instagram"};
+        Integer[]img={R.drawable.ic_launcher_background,R.drawable.ic_launcher_foreground};
+
+        MyListAdapter adapter=new MyListAdapter(this, name,img);
+        lv.setAdapter(adapter);
         btn=findViewById(R.id.learn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,4 +61,19 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public class MyListAdapter extends ArrayAdapter<String> {
+
+        private final Activity context;
+        private final String[] name;
+        private final Integer[] img;
+
+        public MyListAdapter(Activity context, String[] name,Integer[] img) {
+            super(context, R.layout.activity_second, name);
+            // TODO Auto-generated constructor stub
+
+            this.context=context;
+            this.img = img;
+            this.name=name;
+        }
 }
